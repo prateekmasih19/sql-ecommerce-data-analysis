ecbc4f7b638b2f5a58d9675fdba03725ad1bbeeb
prateekmasih19 <122875431+prateekmasih19@users.noreply.github.com>
2025-12-16 10:00:10 +0530

    Update sample_data.sql

--- sample_data.sql
+++ sample_data.sql
@@ -1,28 +1,27 @@
 -- Insert statements with sample data
-CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY,
-    customer_name VARCHAR(50),
-    signup_date DATE
-);
+INSERT INTO Customers VALUES
+(1, 'Amit', '2023-01-05'),
+(2, 'Neha', '2023-01-10'),
+(3, 'Rahul', '2023-02-01'),
+(4, 'Priya', '2023-02-15');
 
-CREATE TABLE Orders (
-    order_id INT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
-);
+INSERT INTO Products VALUES
+(1, 'Laptop', 'Electronics'),
+(2, 'Mobile', 'Electronics'),
+(3, 'Headphones', 'Accessories'),
+(4, 'Keyboard', 'Accessories');
 
-CREATE TABLE Products (
-    product_id INT PRIMARY KEY,
-    product_name VARCHAR(50),
-    category VARCHAR(50)
-);
+INSERT INTO Orders VALUES
+(101, 1, '2023-03-01'),
+(102, 1, '2023-04-01'),
+(103, 2, '2023-03-05'),
+(104, 3, '2023-03-10'),
+(105, 4, '2023-03-20');
 
-CREATE TABLE Order_Items (
-    order_id INT,
-    product_id INT,
-    quantity INT,
-    price INT,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (product_id) REFERENCES Products(product_id)
-);
+INSERT INTO Order_Items VALUES
+(101, 1, 1, 50000),
+(101, 3, 1, 2000),
+(102, 2, 1, 30000),
+(103, 4, 1, 1500),
+(104, 3, 2, 2000),
+(105, 2, 1, 28000);
