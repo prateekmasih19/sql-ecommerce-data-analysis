63af79b0207d3dac5cd64d682ebd9a2ffee5b848
prateekmasih19 <122875431+prateekmasih19@users.noreply.github.com>
2025-12-16 09:59:21 +0530

    Update sample_data.sql

--- sample_data.sql
+++ sample_data.sql
@@ -1,1 +1,28 @@
 -- Insert statements with sample data
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(50),
+    signup_date DATE
+);
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(50),
+    category VARCHAR(50)
+);
+
+CREATE TABLE Order_Items (
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    price INT,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
